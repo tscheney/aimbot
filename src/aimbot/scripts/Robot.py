e11994c6122dc7238555bb14a88ccf21f6a1d072
robot-09 <robot@gmail.com>
2017-02-27 23:24:49 -0700

    Changed wheel velocities to float

--- src/aimbot/scripts/Robot.py
+++ src/aimbot/scripts/Robot.py
@@ -26,7 +26,7 @@ class Robot(Moving):
         self.role = 0
         self.team_side = team_side
         self.vel = (0.0,0.0,0.0) # (vx, vy, w)
-        self.wheel_vel = [0,0,0] # (wheel1, wheel2, wheel3)
+        self.wheel_vel = [0.0,0.0,0.0] # (wheel1, wheel2, wheel3)
         self.publishers = dict()
         self.init_publsihers()
 
