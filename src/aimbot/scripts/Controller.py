9b02b13f2162b8a566a49027defa6d2c57dca3e9
robot-09 <robot@gmail.com>
2017-03-01 04:16:20 -0700

    0 theta

--- src/aimbot/scripts/Controller.py
+++ src/aimbot/scripts/Controller.py
@@ -34,7 +34,7 @@ class Controller:
         yI = gains['y']['I'] if gains is not None else 0
         yD = gains['y']['D'] if gains is not None else 0
 
-        thetaP = gains['theta']['P'] if gains is not None else 2.5
+        thetaP = gains['theta']['P'] if gains is not None else 0
         thetaI = gains['theta']['I'] if gains is not None else 0
         thetaD = gains['theta']['D'] if gains is not None else 0
 
