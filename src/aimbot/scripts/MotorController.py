c569d7cad0c717dc633fd3d39040a772154a6c64
robot-09 <robot@gmail.com>
2017-03-01 02:48:19 -0700

    PIQ update

--- src/aimbot/scripts/MotorController.py
+++ src/aimbot/scripts/MotorController.py
@@ -6,7 +6,7 @@ import math
 
 class MotorController:
     """This class interfaces with the motors via the serial connection to the PSoC"""
-    def __init__(self, p=2, i=0.7, qpps=450):
+    def __init__(self, p=1, i=1, qpps=1100):
         self.ser = serial.Serial()
         self.serial_conn = False
         self.PULSE_PER_ROTATION = 116.2
