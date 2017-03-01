48a0834a8ec25d8905d3a26b9a0cbb645c42d641
robot-09 <robot@gmail.com>
2017-03-01 04:38:14 -0700

    piq

--- src/aimbot/scripts/MotorController.py
+++ src/aimbot/scripts/MotorController.py
@@ -57,7 +57,7 @@ class MotorController:
                 reset = True
 
             if (s1 < 100 and s2 < 100 and s3 < 100):
-                self.setPID(0, 1, 1, 10)
+                self.setPID(0, 1, 1, 75)
                 reset = True
 
             self.writeFloat(s1)
