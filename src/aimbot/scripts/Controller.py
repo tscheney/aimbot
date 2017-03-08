Controller actually uses radians, fixed stuff

src/aimbot/scripts/Controller.py
@@ -121,8 +121,8 @@ class Controller:
 
     def adjust_for_period(self):
         """This function accounts for the periodicity of theta. The robot should never need to rotate more than 180 degrees."""
-        curr = np.deg2rad(self.th_obsv.xhat[0, 0]) # get theta velocity
-        curr_d = np.deg2rad(self.th_obsv.d)
+        curr = self.th_obsv.xhat[0, 0] # get theta velocity
+        curr_d = self.th_obsv.d
         i = 0
         while curr - curr_d < -1 * np.pi:
             curr = curr + 2 * np.pi

src/aimbot/scripts/Robot.py
@@ -114,11 +114,11 @@ class Robot(Moving):
     def update(self):
         """Updates the robots controller and sets velocities"""
         self.determine_des_pos()
-        self.controller.update_des_pos(self.des_pos.x, self.des_pos.y, self.des_pos.theta)
-        self.controller.update_cur_pos(self.pos.x, self.pos.y, self.pos.theta)
+        self.controller.update_des_pos(self.des_pos.x, self.des_pos.y, np.deg2rad(self.des_pos.theta))
+        self.controller.update_cur_pos(self.pos.x, self.pos.y, np.deg2rad(self.pos.theta))
         #print(self.vel)
         self.controller.update()
-        self.vel = self.controller.vel
+        self.vel = [self.controller.vel.x, self.controller.vel.y, np.rad2deg(self.controller.vel.theta)]
         self.wheel_vel = self.controller.wheel_vel
 
     def determine_des_pos(self):