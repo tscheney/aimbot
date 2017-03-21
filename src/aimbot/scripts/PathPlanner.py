dabdb035a259af04a8dc76324e767935992a7246
robot-09 <robot@gmail.com>
2017-03-21 15:36:13 -0600

    worked on path planning

--- src/aimbot/scripts/PathPlanner.py
+++ src/aimbot/scripts/PathPlanner.py
@@ -15,8 +15,6 @@ class PathPlanner:
         self.obj_to_avoid = [] # this is a list of objects to avoid
         self.waypoints = [] #list of points to pass through
         self.ball = Position() #need the ball position
-        self.goal = Position() #the position of the goal
-        self.hand = Position() #center most foward point on robot
         self.point = Position() #intermediate point with angle to face
 
     def update_pos(self, pos):
@@ -29,46 +27,71 @@ class PathPlanner:
 
     def update_obj_avoid(self, obj_to_avoid):
         """Updates the objects to avoid list with a list passed as a parameter"""
-        self.obj_to_avoid.clear() # clears the old positions from the list before adding the new ones
+        i = 0
+        del self.obj_to_avoid[:] # clears the old positions from the list before adding the new ones
         for pos in obj_to_avoid:
+
+            print(obj_to_avoid[i].x, obj_to_avoid[i].y)
             self.obj_to_avoid.append(pos)  # add all the new positions
+            i = i + 1
 
     #TODO here down is new or modified
-    def update_goal(self):
-        self.goal.x = 1.5 #not sure on the coordinates might be 2
-        self.goal.y = 0 #center of the goal
-        self.goal.theta = 0 #doesnt really matter
-
-    def calc_waypoints(self):
+    def calc_waypoints(self, curr_position, des_position, obj_to_avoid):
         """Calculates intermediate position to head to"""
-        self.update_goal()
-        rad = np.radians(self.pos.theta)
-        values = self.hand.x*(np.matrix([[np.cos(rad)], np.sin(rad)]))
+        #need to calculate where to go next one point at a time, pass this back and go there then call this again
+        #model robots as circles to make it easier
+        #this site seems useful http://devmag.org.za/2009/04/13/basic-collision-detection-in-2d-part-1/
 
-        self.point.x = values[0, 0]
-        self.point.y = values[0, 1]
-        print(self.point.x,self.point.y)
-        print(self.goal.x, self.goal.y)
-        # add the point to the list
+        #update where we are and where we want to go
+        self.update_pos(curr_position)
+        self.update_des_pos(des_position)
+        #update things to avoid
+        self.update_obj_avoid(obj_to_avoid)
+
+        robot_radius = 0.1016 #4 inches in meters asuming this is true of all robots
+        distX = self.pos.x - self.obj_to_avoid[0].x
+        self.point = des_position
         self.add_waypoint()
-        #calculate the x and y of our next point
-        self.point.x = self.ball.x - 2*self.hand.x*(self.goal.x-self.ball.x)/(abs(self.goal.x+self.ball.x))
-        self.point.y = self.ball.y - 2*self.hand.y*(self.goal.y-self.ball.y)/(abs(self.goal.y+self.ball.y))
-        self.add_waypoint()
-        self.point = self.goal
-        self.add_waypoint()
+
+
+
+
+
+
+
+
 
     def add_waypoint(self):
         self.waypoints.append(self.point)
+        print("added the following point")
+        print(self.point.x, self.point.y, self.point.theta)
 
     def waypoint_clear(self):
         """clears the waypoints from the list"""
-        self.waypoints.clear()
+        del self.waypoints[:]
 
     def print_waypoints(self, waypoints):
         """prints out the waypoints for debugging purposes"""
-        for self.point in waypoints:
-            print(self.point.x, self.point.y)
+        i = 0
+        #self.point.x = 1
+        #self.point.y = 1
+        #self.point.theta = 1
+        #self.add_waypoint()
+
+        #self.point.x = 2
+        #self.point.y = 2
+        #self.point.theta = 2
+        #self.add_waypoint()
+
+        #self.point.x = 3
+        #self.point.y = 4
+        #self.point.theta = 5
+        #self.add_waypoint()
+
+        print("list starts here, this only prints the last item the number of items in the list times.")
+        for point in waypoints:
+            print(point.x, point.y, point.theta, "index ->", i)
+            i = i +1
 
 # notes on waypoint path finding from http://rwbclasses.groups.et.byu.net/lib/exe/fetch.php?media=robot_soccer:material:waypoints.pdf
 #   1.) He suggests having a few functions, one that will clear the waypoint
--- src/aimbot/scripts/test_pathplanner.py
+++ src/aimbot/scripts/test_pathplanner.py
@@ -4,33 +4,47 @@ from PathPlanner import PathPlanner
 
 path_plan = PathPlanner()
 
+
+
+
 waypoints = []
 robotopp1 = Position()
+opp2 = Position()
+
 ball = Position()
+objects = []
+
 
 robotopp1.x = 1
 robotopp1.y = -0.5
 
-path_plan.obj_to_avoid.append(robotopp1)
+opp2.x = 0
+opp2.y = 1.2
+objects.append(robotopp1)
+objects.append(opp2)
 
+#path_plan.obj_to_avoid.append(robotopp1)
+
+#where we want to go
 des_pos = Position()
 des_pos.x = 1.5
 des_pos.y = -0.5
+#where we are
 pos = Position()
 pos.x = -1
 pos.y = -0.5
 
-path_plan.des_pos.x = 1.5
-path_plan.des_pos.y = -0.5
+#path_plan.des_pos.x = 1.5
+#path_plan.des_pos.y = -0.5
 
-path_plan.pos.x = -1
-path_plan.pos.y = -0.5005
+#path_plan.pos.x = -1
+#path_plan.pos.y = -0.5005
 
-ball.x = 0.1
-ball.y = 0.1
+#ball.x = 0.1
+#ball.y = 0.1
+#path_plan.ball = ball
 
-path_plan.calc_waypoints()
-
+path_plan.calc_waypoints(pos, des_pos, objects)
 waypoints = path_plan.waypoints
 
 path_plan.print_waypoints(waypoints)
