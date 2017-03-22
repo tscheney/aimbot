2cd6cf3d81e7f24874a6e0f2975adc6f422ae7a4
robot-09 <robot@gmail.com>
2017-03-21 20:04:24 -0600

    bugs...

--- src/aimbot/scripts/Robot.py
+++ src/aimbot/scripts/Robot.py
@@ -201,24 +201,24 @@ class Robot(Moving):
         #    cmdvec = p
 
         if ball.x < me.x:
-            if ball.y + .3 < 1.12:
+            if (ball.y + .3) < 1.12:
                 if ball.x -.3 > -1.595:
                     x = ball.x-.3
                     y = ball.y+.3
-                    cmdvec = np.array([x], [y])
+                    cmdvec = np.array([[x], [y]])
                 else:
                     x = ball.x + .05
                     y = ball.y+.3
-                    cmdvec = np.array([x], [y])
+                    cmdvec = np.array([[x], [y]])
             else:
                 if ball.x -.3 > -1.595:
                     x = ball.x -.3
                     y = ball.y -.3
-                    cmdvec = np.array([x], [y])
+                    cmdvec = np.array([[x], [y]])
                 else:
                     x = ball.x + .05
                     y = ball.y -3
-                    cmdvec = np.array([x], [y])
+                    cmdvec = np.array([[x], [y]])
         elif np.linalg.norm(p - mevec) < 0.21:
             cmdvec = goalvec
         else:
