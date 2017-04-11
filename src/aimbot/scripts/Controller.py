586f2ca47eee6ce2504c2f7110927b9109d89dc7
tscheney <thomascheneyyzs@gmail.com>
2017-04-11 03:04:53 -0600

    asdf

--- src/aimbot/scripts/Controller.py
+++ src/aimbot/scripts/Controller.py
@@ -3,8 +3,11 @@ from Observer import Observer
 from MotorController import MotorController
 from Position import Position
 
+#xy_scale = 2
+#xy_max_step = 0.22
+
 xy_scale = 2
-xy_max_step = 0.22
+xy_max_step = 0.11
 xP = {
     'A': np.matrix([[0, 1], [0, -0.0006]]),  # A matrix for observer
     'B': np.matrix([[0], [0.5]]),  # B matrix for observer
