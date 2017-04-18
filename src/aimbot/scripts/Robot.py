f57a84141f197809dce2a89153d98e1eb3d46205
tscheney <thomascheneyyzs@gmail.com>
2017-04-18 02:18:14 -0600

    debug

--- src/aimbot/scripts/Robot.py
+++ src/aimbot/scripts/Robot.py
@@ -327,9 +327,9 @@ class Robot(Moving):
         if(not intersect.is_empty): # if there is an intersection
             goal_pos = Position()
             goal_pos.update(intersect.x, intersect.y, 0)
-            tol = 0.5
+            tol = 0.1
             in_tol = self.dis_from_point_to_line(self.pos, self.ball_pos, goal_pos) < tol
-            if (self.theta_within_error(3) and in_tol):
+            if (self.theta_within_error(5) and in_tol):
             #if (self.theta_within_error(3)):
                 self.attack_ball()
             else:
